2836d3b86aa5b4ed661cc0a0afa5638890f465e8
siddiq <siddiq235@gmail.com>
2016-01-31 18:31:10 -0800

    updated with dish detail and customer comments

--- ionic/conFusion/www/js/app.js
+++ ionic/conFusion/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers'])
+angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -32,11 +32,12 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.home', {
+   .state('app.home', {
     url: '/home',
     views: {
       'mainContent': {
-        templateUrl: 'templates/home.html'
+        templateUrl: 'templates/home.html',
+          controller: 'IndexController'
       }
     }
   })
@@ -59,12 +60,12 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
       }
     })
 
-    .state('app.menu', {
+      .state('app.menu', {
       url: '/menu',
       views: {
         'mainContent': {
           templateUrl: 'templates/menu.html',
-          controller: ''
+          controller: 'MenuController'
         }
       }
     })
@@ -74,7 +75,7 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
     views: {
       'mainContent': {
         templateUrl: 'templates/dishdetail.html',
-        controller: ''
+        controller: 'DishDetailController'
       }
     }
   });
