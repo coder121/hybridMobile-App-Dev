df0a61bef021c9a014b2dbb272bff85a466b34fc
siddiq <siddiq235@gmail.com>
2016-02-14 03:19:07 -0800

    added favorite dish options

--- ionic/conFusion/www/js/app.js
+++ ionic/conFusion/www/js/app.js
@@ -70,6 +70,16 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
         }
       }
     })
+  
+   .state('app.favorites', {
+      url: '/favorites',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/favorites.html',
+            controller:'FavoritesController'
+        }
+      }
+    })
 
   .state('app.dishdetails', {
     url: '/menu/:id',
