0748dfd9f13633f618ea65a23c38cefc071613cc
siddiq <siddiq235@gmail.com>
2016-02-13 23:11:58 -0800

    added ionic list and favorite button

--- ionic/conFusion/www/js/controllers.js
+++ ionic/conFusion/www/js/controllers.js
@@ -67,7 +67,7 @@ angular.module('conFusion.controllers', [])
   };    
 })
 
-.controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+.controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
 
             $scope.baseURL = baseURL;
             
@@ -111,6 +111,12 @@ angular.module('conFusion.controllers', [])
             $scope.toggleDetails = function() {
                 $scope.showDetails = !$scope.showDetails;
             };
+        $scope.addFavorite = function (index) {
+        console.log("index is " + index);
+        favoriteFactory.addToFavorites(index);
+        $ionicListDelegate.closeOptionButtons();
+        };
+    
         }])
 
         .controller('ContactController', ['$scope', function($scope) {
